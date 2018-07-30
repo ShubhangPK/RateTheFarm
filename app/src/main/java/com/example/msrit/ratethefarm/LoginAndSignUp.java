0efb8c519cb414349471ea7e5b813118770711a9
DESKTOP-5PGTGEF\rohit <rohitnagraj.99@gmail.com>
2018-07-30 13:40:52 +0530

    Added buttons to navigate the app for easy development, added all the necessary activities to build in, do not create another activity unless necessary.

--- app/src/main/java/com/example/msrit/ratethefarm/LoginAndSignUp.java
+++ app/src/main/java/com/example/msrit/ratethefarm/LoginAndSignUp.java
@@ -1,13 +1,59 @@
 package com.example.msrit.ratethefarm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginAndSignUp extends AppCompatActivity {
+
+    // Remove when project is done
+    private Button mPage1, mPage2, mPage3, mPage4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_and_sign_up);
+
+        mPage1 = findViewById(R.id.page_1);
+        mPage2 = findViewById(R.id.page_2);
+        mPage3 = findViewById(R.id.page_3);
+        mPage4 = findViewById(R.id.page_4);
+
+        mPage1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent myIntent = new Intent(v.getContext(), LoginAndSignUp.class);
+                startActivity(myIntent);
+            }
+        });
+
+        mPage2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(v.getContext(), GetFarmerDetails.class);
+                startActivity(myIntent);
+            }
+        });
+
+        mPage3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent myIntent = new Intent(v.getContext(), FarmersList.class);
+                startActivity(myIntent);
+            }
+        });
+
+        mPage4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(v.getContext(), ShowFarmerDetails.class);
+                startActivity(myIntent);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/msrit/ratethefarm/ShowFarmerDetails.java
@@ -0,0 +1,13 @@
+package com.example.msrit.ratethefarm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowFarmerDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_farmer_details);
+    }
+}
