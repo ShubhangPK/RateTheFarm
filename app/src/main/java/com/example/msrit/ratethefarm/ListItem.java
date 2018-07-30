d66c30fe9970934347ea96cc67743cdf73651b52
DESKTOP-5PGTGEF\rohit <rohitnagraj.99@gmail.com>
2018-07-30 22:00:42 +0530

    Implementing RecyclerView in page 3

---
+++ app/src/main/java/com/example/msrit/ratethefarm/ListItem.java
@@ -0,0 +1,20 @@
+package com.example.msrit.ratethefarm;
+
+public class ListItem {
+
+    private String head;
+    private String body;
+
+    public ListItem(String head, String body) {
+        this.head = head;
+        this.body = body;
+    }
+
+    public String getHead() {
+        return head;
+    }
+
+    public String getBody() {
+        return body;
+    }
+}
---
+++ app/src/main/java/com/example/msrit/ratethefarm/MyAdapter.java
@@ -0,0 +1,35 @@
+package com.example.msrit.ratethefarm;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_item, parent, false);
+                return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
